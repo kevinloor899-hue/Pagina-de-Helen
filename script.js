87267b837eb9f91d15f89d2314141bff3a707b62
kevinloor899-hue <kevinloor899@gmail.com>
2026-06-17 23:32:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarCarta() {
+    document.getElementById("carta").style.display = "block";
+}
+function irSorpresa() {
+    window.location.href = "carta.html";
+}
+
+function volver() {
+    window.location.href = "index.html";
+}
